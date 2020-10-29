1e3e7fb2de5cd7ecccdedde23f82a3000deb2aac
Chufan Chen <allenplato28@gmail.com>
2020-10-29 22:21:37 +0800

    CI should work now?

--- ex-regincr/ex-regincr.mk
+++ ex-regincr/ex-regincr.mk
@@ -10,9 +10,7 @@ ex_regincr_srcs = \
 ex_regincr_test_srcs = \
   ex-regincr-RegIncr.t.v \
 
-ex_regincr_sim_srcs = \
-  ex-regincr-RegIncr.t.v \
+ex_regincr_sim_srcs = 
 
-ex_regincr_opt_srcs = \
-  ex-regincr-RegIncr-adhoc-test.v \
+ex_regincr_opt_srcs = 
 
